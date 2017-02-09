a452e3cc88426795379e4f5e97d21759fada7880
jutao <573723112@qq.com>
2017-02-09 17:42:24 +0800

    Lambda

--- Java8Action/inaction/src/main/java/com/example/chapter3/LambdaTest.java
+++ Java8Action/inaction/src/main/java/com/example/chapter3/LambdaTest.java
@@ -4,7 +4,9 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 /**
@@ -12,6 +14,23 @@ import java.util.function.Predicate;
  */
 public class LambdaTest {
     public static void main(String[] args) {
+//        lambdaTest();
+//        predicateTest();
+        consumerTest();
+    }
+
+    private static void consumerTest() {
+        Consumer<Integer> consumer = System.out::println;
+        forEach(Arrays.asList(1,2,3,4),consumer);
+    }
+
+    private static <T> void forEach(List<T> list, Consumer<T> c) {
+        for (T t : list) {
+            c.accept(t);
+        }
+    }
+
+    private static void lambdaTest() {
         try {
             //读取第一行
             System.out.println(processFile((BufferedReader br) -> br.readLine()));
@@ -20,9 +39,11 @@ public class LambdaTest {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
-        Predicate<String> nonEmptyStringPredicate = (String s) -> s!=null&&!s.isEmpty();
-        List<String> list=new ArrayList<>();
+    private static void predicateTest() {
+        Predicate<String> nonEmptyStringPredicate = (String s) -> s != null && !s.isEmpty();
+        List<String> list = new ArrayList<>();
         list.add("sss");
         list.add(null);
         list.add("");
@@ -31,9 +52,8 @@ public class LambdaTest {
         for (String s : filter) {
             System.out.println(s);
         }
-
-
     }
+
     public static <T> List<T> filter(List<T> list, Predicate<T> p) {
         List<T> result = new ArrayList<>();
         for (T e : list) {
@@ -43,6 +63,7 @@ public class LambdaTest {
         }
         return result;
     }
+
     public static String processFile(BufferedReaderProcessor p) throws IOException {
         //Java7 中的带资源try语句，我们不需要显式的关闭资源
         try (BufferedReader br = new BufferedReader(new FileReader("E:\\data.txt"))) {
